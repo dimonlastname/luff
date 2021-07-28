24ef4ad8d1375562488dfab7b18a9d0ed09abbd6
Dimon Lastname <asasdasas@yandex.ru>
2021-07-28 16:11:54 +0300

    Update DateBox.tsx

--- luff-comp/src/Input/DateBox.tsx
+++ luff-comp/src/Input/DateBox.tsx
@@ -31,6 +31,8 @@ export default class DateBox extends InputBoxBase<TProps> {
     private minSt;
     private maxSt;
 
+    private PeriodPicker: PeriodPicker;
+
     protected BeforeBuild(): void {
         super.BeforeBuild();
         const {min, max} = this.props;
@@ -39,6 +41,9 @@ export default class DateBox extends InputBoxBase<TProps> {
         this.isMaxState = max instanceof State;
         this.minSt = min as IObservableStateSimple<Date>;
         this.maxSt = max as IObservableStateSimple<Date>;
+    }
+    AfterBuild(): void {
+        this.PeriodPicker = this.GetComponentByName('PeriodPicker');
     }
 
 
@@ -60,33 +65,32 @@ export default class DateBox extends InputBoxBase<TProps> {
         if (this._IsDisabled.SValue)
             return;
         const date = this.props.value.SValue;
-        PeriodPicker.GlobalSinglePicker.Run(date, date, ((dateStart, dateFinish) => {
-            if (this.props.onChange) {
-                this.props.onChange(dateStart.Date);
-                return;
-            }
-            this.props.value.SValue = dateStart.Date;
-        }), {
-            dateMin: this.props.min ? Luff.Date(this.GetMin()) : void 0,
-            dateMax: this.props.max ? Luff.Date(this.GetMax()) : void 0,
-            isShowTimePick: this.props.isTimePick
-        });
+        this.PeriodPicker.Run(date, date, null);
     }
     Render(): any {
         let classState = this._IsDisabled.SubState(isDis => 'l-textbox ' + this.props.className + (isDis ? '': ' l-pointer'));
-        let children = this.props.children;
+        const dateMin = this.props.min ? Luff.Date(this.GetMin()) : void 0;
+        const dateMax = this.props.max ? Luff.Date(this.GetMax()) : void 0;
+
+        const luffDate = Luff.Date(this.props.value.SValue);
+
         return (
             <div className={classState}
                  onClick={() => this.CallDatePicker()}
-                 //value={this.props.value}
-                 // onChange={e => {
-                 //       let value = e.target.value;
-                 //       if (this.props.onChange) {
-                 //           return this.props.onChange(value);
-                 //       }
-                 //       this.props.value.SValue = value;
-                 //   }}
             >
+                <PeriodPicker
+                    dates={[luffDate, luffDate]}
+                    dateMin={dateMin}
+                    dateMax={dateMax}
+                    isOnlyDay={true}
+                    onChange={(dateStart, dateFinish) => {
+                        if (this.props.onChange) {
+                            this.props.onChange(dateStart.Date);
+                            return;
+                        }
+                        this.props.value.SValue = dateStart.Date;
+                    }}
+                />
                 {this.props.value.SubState(x => x ? Luff.Date(x).Format(this.props.format) : this.props.placeholder)}
             </div>
         )
