b05b1009cc4ab71af0247e069d098b1ea7951190
Dimon Lastname <asasdasas@yandex.ru>
2022-03-17 10:49:18 +0300

    Update Paginator.tsx

--- luff-comp/src/Complex/Paginator/Paginator.tsx
+++ luff-comp/src/Complex/Paginator/Paginator.tsx
@@ -1,4 +1,4 @@
-import Luff, {React, EachPaging} from "luff";
+import Luff, {React, EachPaging, IObservableState, IObservableStateSimple} from "luff";
 import ComboBox from "../../Input/ComboBox/ComboBox";
 
 import "./Paginator.scss";
@@ -15,59 +15,106 @@ export default class Paginator extends Luff.ComponentSimple<TProps> {
         className: '',
         pageSizeList: [20, 50, 100],
     };
+    public PagesCount: IObservableStateSimple<number>;
+    public CurrentPage: IObservableStateSimple<number>;
+    public ShownFrom: IObservableStateSimple<number>;
+    public ShownTo: IObservableStateSimple<number>;
+    public get Paging() : EachPaging {
+        return this.props.paging;
+    }
+    public NextPage() {
+        this.Paging.NextPage();
+    }
+    public PrevPage() {
+        this.Paging.PrevPage();
+    }
+    public FirstPage() {
+        this.Paging.Paging.Skip.SValue = 0
+    }
+    public LastPage() {
+        this.Paging.LastPage();
+    }
 
-
-    Render(): any {
+    protected BeforeBuild(): void {
+        super.BeforeBuild();
         const paging = this.props.paging;
+        const p = paging.Paging;
 
-        const pageCount = paging.Paging.Take.SubState(take => {
+        const pageCount = p.Take.SubState(take => {
             if (take == 0)
                 return 0;
-            const count = paging.Paging.Count.SValue;
+            const count = p.Count.SValue;
             return Math.ceil(count / take);
-        }, [paging.Paging.Count]);
-        const page = paging.Paging.SubState(p => {
+        }, [p.Count]);
+        const page = p.SubState(p => {
             let pageIndex = 1 + Math.ceil(p.Skip / p.Take );
             if (!isFinite(pageIndex))
                 pageIndex = 1;
+            if (pageIndex > pageCount.SValue )
+                pageIndex = pageCount.SValue;
             return pageIndex;
         });
 
-        const shownFrom = paging.Paging.Skip.SubState(skip => skip + 1);
-        const shownTo = paging.Paging.Skip.SubState(skip => skip + paging.Paging.Shown.SValue, [paging.Paging.Shown]);
+        const shownFrom = p.Skip.SubState(skip => {
+            let from = skip + 1;
+            if (from > p.Count.SValue) {
+                from = p.Count.SValue - p.Take.SValue;
+            }
+            if (from < 1)
+                from = 1;
+            return from
 
-        if (!this.props.pageSizeList.includes(paging.Paging.Take.SValue)){
-            this.props.pageSizeList.push(paging.Paging.Take.SValue);
+        }, [p.Take, p.Count]);
+        //const shownTo = p.Skip.SubState(skip => skip + p.Shown.SValue, [p.Shown, p.Take]);
+        const shownTo = p.Skip.SubState(skip => {
+            let shown = skip + p.Take.SValue;
+            if (shown > p.Count.SValue)
+                shown = p.Count.SValue;
+            return shown;
+        }, [p.Take, p.Count]);
+
+        if (!this.props.pageSizeList.includes(p.Take.SValue)){
+            this.props.pageSizeList.push(p.Take.SValue);
             this.props.pageSizeList.sort((a, b) => a - b);
         }
+        this.PagesCount = pageCount;
+        this.CurrentPage = page;
+        this.ShownFrom = shownFrom;
+        this.ShownTo = shownTo;
+    }
+
+    protected AfterBuild(): void {
+        //this.props.paging._IsPagingRefreshLocked
+    }
+
+    Render(): Luff.Node {
+        const paging = this.Paging;
+        const p = paging.Paging;
 
         return (
             <div className={"l-paginator " + this.props.className}>
                 <div className="l-paginator-elem">
-                    <button className="l-button button" onClick={() => paging.Paging.Skip.SValue = 0}>&lt;&lt;</button>
+                    <button className="l-button button" onClick={() => this.FirstPage() }>&lt;&lt;</button>
                 </div>
                 <div className="l-paginator-elem">
-                    <button className="l-button button" onClick={() => paging.PrevPage()}>&lt;</button>
+                    <button className="l-button button" onClick={() => this.PrevPage() }>&lt;</button>
                 </div>
-                <div class="l-paginator-elem">Стр. {page} из {pageCount} </div>
+                <div class="l-paginator-elem">Стр. {this.CurrentPage} из {this.PagesCount} </div>
                 <div className="l-paginator-elem">
-                    <button className="l-button button" onClick={() => paging.NextPage()}>&gt;</button>
+                    <button className="l-button button" onClick={() => this.NextPage() }>&gt;</button>
                 </div>
                 <div className="l-paginator-elem">
-                    <button className="l-button button" onClick={() => paging.LastPage() }>&gt;&gt;</button>
+                    <button className="l-button button" onClick={() => this.LastPage() }>&gt;&gt;</button>
                 </div>
                 <div className="l-paginator-elem l-paginator-item-take l-row l-flexa-center">
                     <div className="div">Показывать по </div>
                     <ComboBox
-                        value={paging.Paging.Take}
+                        value={p.Take}
                         dataStatic={this.props.pageSizeList}
                         notFoundValue=""
                     />
                 </div>
-                <div class="l-paginator-elem">Показано {shownFrom}-{shownTo} из {paging.Paging.Count}</div>
-                {/*<div className="l-paginator-children">*/}
-                    {/*{this.props.children}*/}
-                {/*</div>*/}
+                <div class="l-paginator-elem">Показано {this.ShownFrom}-{this.ShownTo} из {p.Count}</div>
                 {this.props.children}
             </div>
         )
